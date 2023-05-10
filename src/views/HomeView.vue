b6a2c295268916d8a5a6fae35bad32291e1be87b
Aleksa Petkovic <99674843+aleksapetkovic25@users.noreply.github.com>
2023-05-10 14:46:05 +0200

    first

---
+++ src/views/HomeView.vue
@@ -0,0 +1,57 @@
+<template>
+  <main>
+    <GameInfoComponent v-if="levelSelected && rivalSelected" :level="levelSelected" :rival="rivalSelected"/>
+    <RivalsComponent @selectedRival="selectedRival" :rivals="rivals"/>
+    <LevelsComponent @selectedLevel="selectedLevel" :rival="rivalSelected"/>
+    
+  </main>
+</template>
+
+
+
+
+<script>
+import RivalsComponent from '../components/RivalsComponent.vue'
+import LevelsComponent from '../components/LevelsComponent.vue'
+import GameInfoComponent from '../components/GameInfoComponent.vue'
+
+export default {
+  components: { RivalsComponent, LevelsComponent, GameInfoComponent},
+  data(){
+    return{
+      rivals: [
+        {
+          img: "avatar-one.png",
+          name: "William Brown"
+        },
+        {
+          img: "avatar-two.png",
+          name: "John Anderson"
+        },
+        {
+          img: "avatar-three.png",
+          name: "Matthew Taylor"
+        }
+      ],
+      levelSelected: null,
+      rivalSelected: null
+    }
+  },
+  methods: {
+    selectedRival(rival){
+      this.rivalSelected = rival;
+    },
+    selectedLevel(level){
+      this.levelSelected = level;
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.test{
+  max-height: 150px;
+  color: white;
+}
+</style>
